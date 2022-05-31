fdb749ced444bd14f2b1eec2f63d2bcbd0ae72ee
a67878813 <len888len888@gmail.com>
2022-05-30 19:57:14 -0700

    message

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -6,6 +6,7 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
 subdirs("catkin_simple")
+subdirs("creating_a_ros_library")
 subdirs("example_ros_class")
 subdirs("example_ros_msg")
 subdirs("my_minimal_nodes")
---
+++ build/creating_a_ros_library/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ea/ros_ws/src/creating_a_ros_library
+# Build directory: /home/ea/ros_ws/build/creating_a_ros_library
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
