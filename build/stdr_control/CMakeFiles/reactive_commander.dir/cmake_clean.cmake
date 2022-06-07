8c04247e887b5807ab6fa04faaf4e9e2d56d9ec9
a67878813 <len888len888@gmail.com>
2022-06-06 17:39:21 -0700

    message

---
+++ build/stdr_control/CMakeFiles/reactive_commander.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reactive_commander.dir/src/reactive_commander.cpp.o"
+  "/home/ea/ros_ws/devel/lib/stdr_control/reactive_commander.pdb"
+  "/home/ea/ros_ws/devel/lib/stdr_control/reactive_commander"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reactive_commander.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
